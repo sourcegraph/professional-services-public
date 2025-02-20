892a7aba34ab8890a86df02254712c890e0ecc94
Travis Lyons <travis.lyons@sourcegraph.com>
2025-02-20 16:39:27 -0500

    examples: Add an OpenAPI Kotlin and Spring Boot 2 migration examples
    Added two example applications demonstrating different Java/Kotlin build scenarios:
    
    1. openapi-kotlin-gradle-example:
    - Demonstrates OpenAPI code generation in a Kotlin Gradle project
    - Uses OpenAPI Generator Gradle plugin to generate API client code
    - Implements a multi-module project structure with buildSrc conventions
    - Includes Moshi for JSON serialization and OkHttp for HTTP client
    
    2. spring-boot-2-gradle-upgrade:
    - Shows migration path from Spring Boot 2.7 to 3.x
    - Demonstrates updated Jakarta EE namespace usage
    - Includes modern security configuration approach
    - Uses H2 database with JPA/Hibernate
    - Implements a simple REST API with product endpoints

---
+++ examples/language/java/openapi-kotlin-gradle-example/build.gradle.kts
@@ -0,0 +1,8 @@
+plugins {
+    id("com.sourcegraph.gradle.scip-java") version "0.1.1"
+}
+
+scipJava {
+    verbose = true
+    output = "index.scip"
+}
---
+++ examples/language/java/spring-boot-2-gradle-upgrade/src/main/java/com/sourcegraph/demo/entity/Product.java
@@ -0,0 +1,23 @@
+package com.sourcegraph.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Double price;
+}
