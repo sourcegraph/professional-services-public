72133ae37eab57f632444e3029554ded4fd9d4cc
Travis Lyons <travis.lyons@sourcegraph.com>
2025-03-03 16:52:35 -0500

    (agent)introduce bad autowired code

--- examples/language/java/spring-boot-2-gradle-upgrade/src/main/java/com/sourcegraph/demo/service/ProductService.java
+++ examples/language/java/spring-boot-2-gradle-upgrade/src/main/java/com/sourcegraph/demo/service/ProductService.java
@@ -2,16 +2,15 @@ package com.sourcegraph.demo.service;
 
 import com.sourcegraph.demo.entity.Product;
 import com.sourcegraph.demo.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
 public class ProductService {
-    private final ProductRepository productRepository;
 
-    public ProductService(ProductRepository productRepository) {
-        this.productRepository = productRepository;
-    }
+    @Autowired
+    private final ProductRepository productRepository;
 
     public List<Product> getAllProducts() {
         return productRepository.findAll();
