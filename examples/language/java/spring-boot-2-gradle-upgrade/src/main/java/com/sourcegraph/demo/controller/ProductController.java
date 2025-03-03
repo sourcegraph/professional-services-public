7426b604944b877d77730ae90fd487a50216cb2e
Travis Lyons <travis.lyons@sourcegraph.com>
2025-03-03 16:51:10 -0500

    revert code changes

--- examples/language/java/spring-boot-2-gradle-upgrade/src/main/java/com/sourcegraph/demo/controller/ProductController.java
+++ examples/language/java/spring-boot-2-gradle-upgrade/src/main/java/com/sourcegraph/demo/controller/ProductController.java
@@ -2,7 +2,6 @@ package com.sourcegraph.demo.controller;
 
 import com.sourcegraph.demo.entity.Product;
 import com.sourcegraph.demo.service.ProductService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,9 +10,11 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/products")
 public class ProductController {
+    private final ProductService productService;
 
-    @Autowired
-    private ProductService productService;
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
 
     @GetMapping
     public List<Product> getAllProducts() {
