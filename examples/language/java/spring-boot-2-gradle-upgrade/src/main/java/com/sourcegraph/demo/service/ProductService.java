b46e29a743aa5def995d517bbc7731658e67df47
Travis Lyons <travis.lyons@sourcegraph.com>
2025-03-03 16:56:43 -0500

    fix the bad code?

--- examples/language/java/spring-boot-2-gradle-upgrade/src/main/java/com/sourcegraph/demo/service/ProductService.java
+++ examples/language/java/spring-boot-2-gradle-upgrade/src/main/java/com/sourcegraph/demo/service/ProductService.java
@@ -10,7 +10,7 @@ import java.util.List;
 public class ProductService {
 
     @Autowired
-    private final ProductRepository productRepository;
+    private ProductRepository productRepository;
 
     public List<Product> getAllProducts() {
         return productRepository.findAll();
