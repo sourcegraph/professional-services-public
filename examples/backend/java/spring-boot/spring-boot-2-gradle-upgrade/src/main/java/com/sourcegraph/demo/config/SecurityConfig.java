da28c6be39eda23848c681fe4bb2ba68097a2257
Sourcegraph <batch-changes@sourcegraph.com>
2025-04-30 20:24:53 +0000

    Migrate to Spring Boot 3.2.4

--- examples/backend/java/spring-boot/spring-boot-2-gradle-upgrade/src/main/java/com/sourcegraph/demo/config/SecurityConfig.java
+++ examples/backend/java/spring-boot/spring-boot-2-gradle-upgrade/src/main/java/com/sourcegraph/demo/config/SecurityConfig.java
@@ -7,7 +7,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig  {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
